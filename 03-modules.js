c34ba0445667cb007ad2f8c25c5328234899693d
lzalit238 <lzalit238@west-mec.org>
2021-10-18 15:20:59 -0700

    In-class walkthrough 1-10

---
+++ 03-modules.js
@@ -0,0 +1,10 @@
+//  CommonJS, every file is a module (by default)
+
+const names = require('./04-name')
+const sayHi = require('./05-utils')
+const data = require('./06-alternative-flavor')
+require('./07-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
+
